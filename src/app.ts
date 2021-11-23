4e0d1f3d204340d66e7c744f0b90f9139ee9dfa6
Kevin Ure <Kevin.M.Ure@gmail.com>
2021-11-23 17:09:19 -0500

    seperated running the express server from the rest endpoint definitions to support testing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,6 @@
-import express, { Request, Response, NextFunction } from 'express';
+import app from './server'
 
-const app = express();
 const port = 3000;
-
-
-const getHelloWorld = (request:Request, response: Response, next: NextFunction) => {
-  response.status(200).json({Greeting: 'Hello World!'});
-}
-
-app.get('/hello', getHelloWorld);
 
 app.listen(port, () => {
   console.log(`Application is listening on port ${port}.`);
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+const getHelloWorld = (request:Request, response: Response, next: NextFunction) => {
+  response.status(200).json({Greeting: 'Hello World!'});
+}
+
+app.get('/hello', getHelloWorld);
+
+export default app;
